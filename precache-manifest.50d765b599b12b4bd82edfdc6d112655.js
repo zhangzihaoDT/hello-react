b60c2c2bd0335596555667ac31ec08ab30b1b3a8
zhangzihao <zhangzihao@dtcj.com>
2019-11-29 15:04:19 +0800

    Updates

---
+++ precache-manifest.50d765b599b12b4bd82edfdc6d112655.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79db05ab680e790ed00d0f22687c31b",
+    "url": "//hello-react/index.html"
+  },
+  {
+    "revision": "638da536dfbca0c394b4",
+    "url": "//hello-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "277180152955a59ab763",
+    "url": "//hello-react/static/js/2.a95c6171.chunk.js"
+  },
+  {
+    "revision": "638da536dfbca0c394b4",
+    "url": "//hello-react/static/js/main.2247842f.chunk.js"
+  },
+  {
+    "revision": "fba06fdb9ed19c6ae52e",
+    "url": "//hello-react/static/js/runtime-main.a51873cd.js"
+  }
+]);
